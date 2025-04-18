52793c0e0030375979157557bc160dee5ecfba66
Darren Jordan <bingopilingo@gmail.com>
2025-04-18 20:51:00 +0100

    Move account page to pages/ to fix prerender issues

--- src/app/account/page.tsx
+++
@@ -1,8 +0,0 @@
-// Import the client-side component dynamically with `ssr: false` NOT allowed here anymore
-// Instead, just render a client component directly
-
-import AccountClient from '@/components/AccountClient'
-
-export default function AccountPage() {
-  return <AccountClient />
-}
