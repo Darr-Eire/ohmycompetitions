ac1c2b470d0a0d410a29374e4e36373008afa323
Darren Jordan <bingopilingo@gmail.com>
2025-04-18 16:04:18 +0100

    Fix: removed duplicate Header/Footer from Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+'use client'
+
 import LoginButton from '@/components/LoginButton'
 import ThousandPiCompetitionCard from '@/components/ThousandPiCompetitionCard'
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main className="flex-grow flex flex-col items-center justify-start p-6 gap-6">
-        <LoginButton />
-        <ThousandPiCompetitionCard />
-      </main>
-
-      <Footer />
-    </>
+    <main className="flex-grow flex flex-col items-center justify-start p-6 gap-6">
+      <LoginButton />
+      <ThousandPiCompetitionCard />
+    </main>
   )
 }
