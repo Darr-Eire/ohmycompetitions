816d813f2f1cf97c632f9f90ec53d1190a778550
Darren Jordan <bingopilingo@gmail.com>
2025-04-18 20:27:31 +0100

    Fix server/client mismatch in account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,4 +1,7 @@
-import AccountClient from '@/components/AccountClient'
+import dynamic from 'next/dynamic'
+
+// Dynamically load the AccountClient only on the client
+const AccountClient = dynamic(() => import('@/components/AccountClient'), { ssr: false })
 
 export default function AccountPage() {
   return <AccountClient />
