62cdc2be373d0329bb5292ecc5b10879d433c63b
Darren Jordan <bingopilingo@gmail.com>
2025-04-19 01:47:08 +0100

    Fix: extend session user to include id

--- src/components/types/next-auth.d.ts
+++ src/components/types/next-auth.d.ts
@@ -1,11 +1,12 @@
-// src/types/next-auth.d.ts
-declare module "next-auth" {
+import NextAuth from 'next-auth'
+
+declare module 'next-auth' {
   interface Session {
     user: {
       id: string
       name?: string | null
       email?: string | null
+      image?: string | null
     }
   }
 }
-
