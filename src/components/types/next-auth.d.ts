aaf70adfaa103759e140c233a566f0760f23f241
Darren Jordan <bingopilingo@gmail.com>
2025-04-19 01:41:53 +0100

    Fix: removed unused NextAuth import to pass Vercel ESLint build

--- src/components/types/next-auth.d.ts
+++ src/components/types/next-auth.d.ts
@@ -1,12 +1,13 @@
-import NextAuth from 'next-auth'
+// src/types/next-auth.d.ts
+import NextAuth from "next-auth"
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     user: {
       id: string
-      name?: string
-      email?: string
-      image?: string
+      name?: string | null
+      email?: string | null
     }
   }
 }
+
