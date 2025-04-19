b063958fe2ed99a304902bbdd451f4e4e311ba1e
Darren Jordan <bingopilingo@gmail.com>
2025-04-19 02:11:31 +0100

    Fix TypeScript error for Pi.createPayment and define global Pi types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,40 +1,25 @@
 export {}
 
 declare global {
-  interface PiUser {
-    uid: string
-    username: string
-  }
-
-  interface PiAuthResult {
-    accessToken: string
-    user: PiUser
-  }
-
-  interface PiPayment {
-    identifier: string
-    amount: number
-    memo: string
-    metadata: Record<string, string>
-    to_address: string
-  }
-
-  interface PiPaymentCallbacks {
-    onReadyForServerApproval: (paymentId: string) => void
-    onReadyForServerCompletion: (paymentId: string, txid: string) => void
-    onCancel: (paymentId: string) => void
-    onError: (error: Error, payment?: PiPayment) => void
-  }
-
   interface Window {
     Pi: {
       authenticate: (
         scopes: string[],
-        onIncompletePaymentFound: (payment: PiPayment) => void
-      ) => Promise<PiAuthResult>
+        onIncompletePaymentFound: (payment: any) => void
+      ) => Promise<any>
+
       createPayment: (
-        paymentData: PiPayment,
-        callbacks: PiPaymentCallbacks
+        paymentData: {
+          amount: string
+          memo: string
+          metadata: object
+        },
+        callbacks: {
+          onReadyForServerApproval: (paymentId: string) => void
+          onReadyForServerCompletion: (paymentId: string, txid: string) => void
+          onCancel: (paymentId: string) => void
+          onError: (error: Error) => void
+        }
       ) => void
     }
   }
