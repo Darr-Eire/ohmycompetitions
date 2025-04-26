c48890c258095998c52ea7fb8921282febd0299f
Darren Jordan <bingopilingo@gmail.com>
2025-04-26 14:54:32 +0100

    Remove experimental app/ folder and revert to pages router

--- src/app/success/page.js
+++
@@ -1,7 +0,0 @@
-export default function SuccessPage() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-green-100">
-      <h1 className="text-3xl font-bold">Thank you! Payment Successful.</h1>
-    </div>
-  );
-}
