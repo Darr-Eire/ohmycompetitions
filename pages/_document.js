845f7868ed566b700dd88db8b45bd43349f8df6f
Darren Jordan <bingopilingo@gmail.com>
2025-04-26 13:54:46 +0100

    Keep Pages Router home and head: remove app/page.js & app/head.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,22 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        {/* Pi SDK */}
-        <script src="https://sdk.minepi.com/pi-sdk.js" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if (window.Pi?.init) Pi.init({ version: '2.0' });
-            `,
-          }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class Document extends React.Component {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Pi SDK loader */}
+          <script
+            async
+            src="https://sdk.minepi.com/pi.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
