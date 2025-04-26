503d9777d2f9d9f114f181056af0e4b7168f3228
Darren Jordan <bingopilingo@gmail.com>
2025-04-26 20:21:52 +0100

    Fix import paths for globals.css, Header, and Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,12 @@
 // src/components/layout.js
-import Header from '../header'
+import Header from './Header'
 import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <Header />
-      <main className="content">
-        {children}
-      </main>
+      <main className="content">{children}</main>
       <Footer />
     </div>
   )
