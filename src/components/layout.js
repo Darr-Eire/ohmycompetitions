e94dde491d33b29c53f10abde9cdf3fa9a07c4c6
Darren Jordan <bingopilingo@gmail.com>
2025-04-26 19:44:21 +0100

    Your commit message describing the updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,20 @@
-// components/Layout.js
+// src/components/Layout.js
+import React from 'react'
+import Head from 'next/head'
+
 export default function Layout({ children }) {
-    return (
-      <>
-        <header>
-          <h1 className="text-xl font-bold">Oh My Competitions</h1>
-        </header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()} Oh My Competitions
-        </footer>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <Head>
+        <title>Oh My Competitions</title>
+      </Head>
+      <header>
+        <h1>Oh My Competitions</h1>
+      </header>
+      <main>{children}</main>
+      <footer>
+        <p>© {new Date().getFullYear()} Oh My Competitions</p>
+      </footer>
+    </>
+  )
+}
