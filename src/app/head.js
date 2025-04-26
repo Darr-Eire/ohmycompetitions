4d088193c820fc4590ac44f26d9f07eb5c52a958
Darren Jordan <bingopilingo@gmail.com>
2025-04-26 12:24:49 +0100

    fix: load Pi SDK via raw script tag in app/head.js

--- src/app/head.js
+++ src/app/head.js
@@ -1,16 +1,17 @@
 // src/app/head.js
-import Script from 'next/script';
 
 export default function Head() {
-  return (
-    <>
-      <title>OhMyCompetitions</title>
-      <meta name="description" content="Pi Network competition platform" />
-      {/* Load the Pi JS widget early */}
-      <Script
-        src="https://sdk.minepi.com/widget.js"
-        strategy="beforeInteractive"
-      />
-    </>
-  );
-}
+    return (
+      <>
+        <title>OhMyCompetitions</title>
+        <meta name="description" content="Pi Network competition platform" />
+        {/* Load Pi widget SDK early via raw script tag */}
+        <script
+          src="https://sdk.minepi.com/widget.js"
+          async
+          defer
+        />
+      </>
+    );
+  }
+  
