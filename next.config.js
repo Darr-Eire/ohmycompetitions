7372105df3090c3b4bfb85dbfac62c03e005e7ac
Darren Jordan <bingopilingo@gmail.com>
2025-05-04 03:05:05 +0100

    Set up next-translate with en, fr, es

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 // next.config.js
+const nextTranslate = require('next-translate')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-  
-    
-  
-    webpack(config) {
-      // Stub out Node.js core modules MongoDB driver depends on,
-      // so Next.js’s client-side bundle build won’t error on import.
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-        dns: false,
-        fs: false,
-        'fs/promises': false,
-        child_process: false,
-        'timers/promises': false,
-      };
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = nextTranslate({
+  reactStrictMode: true,
+
+  webpack(config) {
+    // Stub out Node.js core modules MongoDB driver depends on,
+    // so Next.js’s client-side bundle build won’t error on import.
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false,
+      tls: false,
+      dns: false,
+      fs: false,
+      'fs/promises': false,
+      child_process: false,
+      'timers/promises': false,
+    }
+    return config
+  },
+})
+
+module.exports = nextConfig
