dbf3408fb1938b3a741b79826cd4bf9c2c1fe283
Darren Jordan <bingopilingo@gmail.com>
2025-05-05 20:53:49 +0100

    Finalize Pi SDK initialization and index.jsx page

--- pages/_app.js
+++ pages/_app.js
@@ -1,48 +1,14 @@
 // pages/_app.js
-import NextApp from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Layout from '@/components/layout'
 import 'styles/globals.css'
-import Script from 'next/script'
 
-class App extends NextApp {
-  static async getInitialProps(appContext) {
-    const appProps = await NextApp.getInitialProps(appContext)
-    return { ...appProps }
-  }
-
-  render() {
-    const { Component, pageProps } = this.props
-    const { session } = pageProps
-
-    return (
-      <SessionProvider session={session}>
-       {/* 1) Load Pi SDK before React mounts */}
-        <Script
-          src="https://sdk.minepi.com/pi-sdk.js"
-          strategy="beforeInteractive"
-        />
-     {/* 2) Immediately init it before hydration */}
-        <Script
-          id="pi-init"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.Pi.init({
-                version: "2.0",
-                sandbox: ${process.env.NODE_ENV !== 'production'}
-              });
-              console.log("✅ Pi SDK initialized");
-            `,
-          }}
-        />
-
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
-    )
-  }
+export default function App({ Component, pageProps: { session, ...rest } }) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...rest} />
+      </Layout>
+    </SessionProvider>
+  )
 }
-
-export default App
--- pages/_document.js
+++ pages/_document.js
@@ -3,20 +3,21 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
+    const sandbox = process.env.NODE_ENV !== 'production'
     return (
       <Html>
         <Head>
-          {/* 1) Load the Pi SDK as soon as the page starts parsing */}
-          <script src="https://sdk.minepi.com/pi-sdk.js" async />
+          {/* 1) Load the Pi SDK as soon as the HTML is parsed */}
+          <script src="https://sdk.minepi.com/pi-sdk.js"></script>
 
-          {/* 2) Initialize it immediately on load */}
+          {/* 2) Initialize it immediately after load */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                if (window.Pi) {
+                if (window.Pi && typeof window.Pi.init === 'function') {
                   window.Pi.init({
                     version: "2.0",
-                    sandbox: ${process.env.NODE_ENV !== 'production'}
+                    sandbox: ${sandbox}
                   });
                   console.log("✅ Pi SDK loaded & initialized");
                 }
