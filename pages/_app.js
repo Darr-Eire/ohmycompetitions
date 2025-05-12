57267d019a94ba1d59ff9073a1d48e8d370e18dc
Darren Jordan <bingopilingo@gmail.com>
2025-05-12 20:59:40 +0100

    Integrate PiAuthContext and header login with Pi

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,9 @@
-// pages/_app.js
-import React from 'react'
-import { SessionProvider } from 'next-auth/react'
-import Layout from '@/components/layout'
-import { PiAuthProvider } from '@/contexts/PiAuthContext'
-import '@fontsource/orbitron' // ✅ Futuristic font
-import 'styles/globals.css'   // ✅ Your global styles
+import { PiAuthProvider } from '@/context/PiAuthContext';
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+export default function RootLayout({ children }) {
   return (
-    <SessionProvider session={session}>
-      <PiAuthProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </PiAuthProvider>
-    </SessionProvider>
-  )
+    <PiAuthProvider>
+      {children}
+    </PiAuthProvider>
+  );
 }
