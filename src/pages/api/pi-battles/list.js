bf74b3b0662534c76e8f0d6b2db0c95daf324477
Darren Jordan <bingopilingo@gmail.com>
2025-06-06 12:06:20 +0100

    Remove battles files and API routes

--- src/pages/api/pi-battles/list.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from '../../../lib/dbConnect';
-import Battle from '../../../models/Battle';
-
-export default async function handler(req, res) {
-  await dbConnect();
-  const battles = await Battle.find({ status: 'open' }).sort({ createdAt: -1 });
-  res.status(200).json(battles);
-}
