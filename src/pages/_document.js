700a223b3028ba97e3195bb1a316745e227c6f1e
darr-eire <ohmycompetitions@gmail.com>
2025-10-15 19:15:43 +0100

    changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,16 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   function initPiOnce(where = 'unknown') {
-    if (typeof window === 'undefined' || !window.Pi || initDoneRef.current) return;
+    if (typeof window === 'undefined' || !window.Pi) return;
     alert("no undef 333")
     try {
       window.Pi.init({ version: '2.0', sandbox: false });
       alert("init done")
-      initDoneRef.current = true;
       window.__piInitDone = true;
-      // eslint-disable-next-line no-console
-      console.info(`[Pi] init OK @ ${where}`, { sandbox: false, appIdPresent: !!APP_ID });
-    } catch (e) {
+    } catch (e:any) {
       // eslint-disable-next-line no-console
       console.error(`[Pi] init error @ ${where}:`, e?.message || e);
     }
