0e92ea1c75fed5829e2813a96a9f8834897fdfd7
darr-eire <ohmycompetitions@gmail.com>
2025-10-15 15:23:55 +0100

    chsnges

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// PATH: src/pages/_document.jsx
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Ensure Pi SDK loads for all browsers */}
+          <script
+            src="https://sdk.minepi.com/pi-sdk.js"
+            async
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
