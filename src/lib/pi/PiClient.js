bd7ad1e95e7cf0cbbf7a0850490a4a4f12609e13
darr-eire <ohmycompetitions@gmail.com>
2025-10-16 03:47:45 +0100

    push it

--- src/lib/pi/PiClient.js
+++ src/lib/pi/PiClient.js
@@ -3,7 +3,8 @@
 // Exposes named exports used by your page.
 // ============================================================================
 import { readyPi } from 'lib/piClient';
-import { PiNetworkService } from 'lib/pi/PiBackendIntegration';
+import { PiNetworkService } from './PiBackendIntegration';
+
 
 
 export function isSandboxEnv() {
