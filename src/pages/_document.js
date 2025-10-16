d7397e6ab05ae400aebe2b5d77fd7bc692c00a00
darr-eire <ohmycompetitions@gmail.com>
2025-10-16 19:17:44 +0100

    changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,33 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+// src/pages/_document.tsx (or .jsx)
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  function initPiOnce(where = 'unknown') {
-    if (typeof window === 'undefined' || !window.Pi) return;
-    alert("no undef 333")
-    try {
-      window.Pi.init({ version: '2.0', sandbox: false });
-      alert("init done")
-      window.__piInitDone = true;
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(`[Pi] init error @ ${where}:`, e?.message || e);
-    }
-  }
-
   return (
     <Html lang="en">
-
       <Head />
       <body>
         <Main />
         <NextScript />
-      <Script
-        id="pi-sdk"
-        src="https://sdk.minepi.com/pi-sdk.js"
-        strategy="beforeInteractive"
-        onLoad={() => initPiOnce('Script.onLoad')}
-      />
       </body>
     </Html>
   );
